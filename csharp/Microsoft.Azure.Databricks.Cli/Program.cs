d72923818bd1ae0e7f1cc1e849d4e443d7a018b8
Jason Wang <jasowang@microsoft.com>
2018-07-26 11:03:00 -0700

    Pass exit code to main

--- csharp/Microsoft.Azure.Databricks.Cli/Program.cs
+++ csharp/Microsoft.Azure.Databricks.Cli/Program.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.Extensions.CommandLineUtils;
-
-namespace Microsoft.Azure.Databricks.Cli
+﻿namespace Microsoft.Azure.Databricks.Cli
 {
     internal class Program
     {
         private static int Main(string[] args)
         {
             var app = new DatabricksCommandLineBuilder().Build();
-            app.Execute(args);
-            return 0;
+            var exitCode = app.Execute(args);
+            return exitCode;
         }
     }
 }
