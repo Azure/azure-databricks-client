f0183229cb8a401a64a556951b5815857a2b1339
Jason Wang <jasowang@microsoft.com>
2018-08-09 23:04:43 -0700

    Remove user name

--- csharp/Sample/Program.cs
+++ csharp/Sample/Program.cs
@@ -14,7 +14,7 @@ namespace Sample
         /// <summary>
         /// Must be an existing user in the databricks environment, otherwise you will get a "DIRECTORY_PROTECTED" error.
         /// </summary>
-        private const string DatabricksUserName = "jasowang@microsoft.com"; 
+        private const string DatabricksUserName = "username@company.com"; 
 
         private static readonly string SampleWorkspacePath = $"/Users/{DatabricksUserName}/SampleWorkspace";
         private static readonly string SampleNotebookPath = $"{SampleWorkspacePath}/Quick Start Using Scala";
