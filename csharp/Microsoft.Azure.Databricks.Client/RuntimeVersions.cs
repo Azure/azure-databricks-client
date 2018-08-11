cc9bd3ad98dd58eff1d1d64d1ae9eae1a431ebaa
Jason Wang <jasowang@microsoft.com>
2018-08-10 19:31:21 -0700

    Remove 'Latest' versions

--- csharp/Microsoft.Azure.Databricks.Client/RuntimeVersions.cs
+++ csharp/Microsoft.Azure.Databricks.Client/RuntimeVersions.cs
@@ -52,16 +52,6 @@ namespace Microsoft.Azure.Databricks.Client
         public const string Runtime_4_2_GPU_Scala_2_11 = "4.2.x-gpu-scala2.11";
 
         /// <summary>
-        /// Latest stable (GPU, Scala 2.11)
-        /// </summary>
-        public const string Runtime_Latest_Stable_GPU_Scala_2_11 = "latest-stable-gpu-scala2.11";
-
-        /// <summary>
-        /// Latest stable (Scala 2.11)
-        /// </summary>
-        public const string Runtime_Latest_Stable_Scala_2_11 = "latest-stable-scala2.11";
-
-        /// <summary>
         /// 4.3 (includes Apache Spark 2.3.1, GPU, Scala 2.11)
         /// </summary>
         public const string Runtime_4_3_GPU_Scala_2_11 = "4.3.x-gpu-scala2.11";
