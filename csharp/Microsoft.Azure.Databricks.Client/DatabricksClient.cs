7eaa91bc13c239c3035c134caed349e61f97c3d5
Jason Wang <jasowang@microsoft.com>
2020-03-06 01:34:10 -0800

    [Preview feature] Support calling Databricks API with AAD service principals (#35)

--- csharp/Microsoft.Azure.Databricks.Client/DatabricksClient.cs
+++ csharp/Microsoft.Azure.Databricks.Client/DatabricksClient.cs
@@ -49,6 +49,52 @@ namespace Microsoft.Azure.Databricks.Client
         /// <summary>
         /// Initializes a new instance of the <see cref="DatabricksClient"/> class.
         /// </summary>
+        /// <remarks>
+        /// The AAD service principal used to acquire the token must be contributor role to the databricks workspace resource.
+        /// </remarks>
+        /// <param name="baseUrl">The base URL of the databricks portal. ex. https://southcentralus.azuredatabricks.net</param>
+        /// <param name="workspaceResourceId">The ResourceId of the databricks workspace.</param>
+        /// <param name="databricksToken">The AAD token used to access the global databricks application (2ff814a6-3304-4ab8-85cb-cd0e6f879c1d).</param>
+        /// <param name="managementToken">The AAD token for Azure management API (https://management.core.windows.net/).</param>
+        /// <param name="timeoutSeconds">The timeout in seconds for the http requests.</param>
+        private DatabricksClient(string baseUrl, string workspaceResourceId, string databricksToken, string managementToken,  long timeoutSeconds = 30)
+        {
+            var apiUrl = new Uri(new Uri(baseUrl), "api/2.0/");
+
+            var handler = new TimeoutHandler
+            {
+                InnerHandler = new HttpClientHandler
+                {
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+                }
+            };
+
+            var httpClient = new HttpClient(handler)
+            {
+                BaseAddress = apiUrl,
+                Timeout = TimeSpan.FromSeconds(timeoutSeconds)
+            };
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", databricksToken);
+            httpClient.DefaultRequestHeaders.Add("X-Databricks-Azure-SP-Management-Token", managementToken);
+            httpClient.DefaultRequestHeaders.Add("X-Databricks-Azure-Workspace-Resource-Id", workspaceResourceId);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            this.Clusters = new ClustersApiClient(httpClient);
+            this.Jobs = new JobsApiClient(httpClient);
+            this.Dbfs = new DbfsApiClient(httpClient);
+            this.Secrets = new SecretsApiClient(httpClient);
+            this.Groups = new GroupsApiClient(httpClient);
+            this.Libraries = new LibrariesApiClient(httpClient);
+            this.Token = new TokenApiClient(httpClient);
+            this.Workspace = new WorkspaceApiClient(httpClient);
+            this.InstancePool = new InstancePoolApiClient(httpClient);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatabricksClient"/> class.
+        /// </summary>
         /// <param name="clusterApi">The cluster API implementation.</param>
         /// <param name="jobsApi">The jobs API implementation.</param>
         /// <param name="dbfsApi">The dbfs API implementation.</param>
@@ -81,6 +127,25 @@ namespace Microsoft.Azure.Databricks.Client
         public static DatabricksClient CreateClient(string baseUrl, string token, long timeoutSeconds = 30)
         {
             return new DatabricksClient(baseUrl, token, timeoutSeconds);
+        }
+
+        /// <summary>
+        /// Create client object with specified base URL, workspace resourceId, databricks token, management API token and timeout.
+        /// This is to support calling Databricks API with an Azure AAD app.
+        /// </summary>
+        /// <remarks>
+        /// This feature is still in preview.
+        /// The AAD service principal used to acquire the token must be contributor role to the databricks workspace resource.
+        /// Request the access token via the AAD oauth2 API (v1): https://docs.microsoft.com/en-us/azure/active-directory/azuread-dev/v1-oauth2-client-creds-grant-flow#request-an-access-token
+        /// </remarks>
+        /// <param name="baseUrl">The base URL of the databricks portal. ex. https://southcentralus.azuredatabricks.net</param>
+        /// <param name="workspaceResourceId">The ResourceId of the databricks workspace.</param>
+        /// <param name="databricksToken">The AAD token used to access the global databricks application (resource to claim: "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d").</param>
+        /// <param name="managementToken">The AAD token for Azure management API (resource to claim: "https://management.core.windows.net/").</param>
+        /// <param name="timeoutSeconds">The timeout in seconds for the http requests.</param>
+        public static DatabricksClient CreateClient(string baseUrl, string workspaceResourceId, string databricksToken, string managementToken, long timeoutSeconds = 30)
+        {
+            return new DatabricksClient(baseUrl, workspaceResourceId, databricksToken, managementToken, timeoutSeconds);
         }
 
         /// <summary>
