c8d9b314122885f327063d315d04f748a5d5df76
Jason Wang <jasowang@microsoft.com>
2023-05-08 01:30:04 -0700

    update runtime versions table (#130)

--- csharp/Microsoft.Azure.Databricks.Client/RuntimeVersions.cs
+++ csharp/Microsoft.Azure.Databricks.Client/RuntimeVersions.cs
@@ -12,21 +12,6 @@ public class RuntimeVersions
     /// 7.3 LTS (includes Apache Spark 3.0.1, Scala 2.12)
     /// </summary>
     public const string Runtime_7_3 = "7.3.x-scala2.12";
-
-    /// <summary>
-    /// 7.3 LTS ML (includes Apache Spark 3.0.1, Scala 2.12)
-    /// </summary>
-    public const string Runtime_7_3_CPU_ML = "7.3.x-cpu-ml-scala2.12";
-
-    /// <summary>
-    /// 7.3 LTS ML (includes Apache Spark 3.0.1, GPU, Scala 2.12)
-    /// </summary>
-    public const string Runtime_7_3_GPU_ML = "7.3.x-gpu-ml-scala2.12";
-
-    /// <summary>
-    /// 7.3 LTS Genomics (includes Apache Spark 3.0.1, Scala 2.12)
-    /// </summary>
-    public const string Runtime_7_3_HLS = "7.3.x-hls-scala2.12";
     #endregion
 
     #region 9.1 LTS
@@ -77,78 +62,6 @@ public class RuntimeVersions
 
     #endregion
 
-    #region 11.0
-
-    /// <summary>
-    /// 11.0 (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_0 = "11.0.x-scala2.12";
-
-    /// <summary>
-    /// 11.0 ML (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_0_CPU_ML = "11.0.x-cpu-ml-scala2.12";
-
-    /// <summary>
-    /// 11.0 ML (includes Apache Spark 3.3.0, GPU, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_0_GPU_ML = "11.0.x-gpu-ml-scala2.12";
-
-    /// <summary>
-    /// 11.0 Photon (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_0_PHOTON = "11.0.x-photon-scala2.12";
-
-    #endregion
-
-    #region 11.1
-
-    /// <summary>
-    /// 11.1 (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_1 = "11.1.x-scala2.12";
-
-    /// <summary>
-    /// 11.1 ML (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_1_CPU_ML = "11.1.x-cpu-ml-scala2.12";
-
-    /// <summary>
-    /// 11.1 ML (includes Apache Spark 3.3.0, GPU, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_1_GPU_ML = "11.1.x-gpu-ml-scala2.12";
-
-    /// <summary>
-    /// 11.1 Photon (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_1_PHOTON = "11.1.x-photon-scala2.12";
-
-    #endregion
-
-    #region 11.2
-
-    /// <summary>
-    /// 11.2 (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_2 = "11.2.x-scala2.12";
-
-    /// <summary>
-    /// 11.2 ML (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_2_CPU_ML = "11.2.x-cpu-ml-scala2.12";
-
-    /// <summary>
-    /// 11.2 ML (includes Apache Spark 3.3.0, GPU, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_2_GPU_ML = "11.2.x-gpu-ml-scala2.12";
-
-    /// <summary>
-    /// 11.2 Photon (includes Apache Spark 3.3.0, Scala 2.12)
-    /// </summary>
-    public const string Runtime_11_2_PHOTON = "11.2.x-photon-scala2.12";
-
-    #endregion
-
     #region 11.3 LTS
 
     /// <summary>
@@ -170,6 +83,102 @@ public class RuntimeVersions
     /// 11.3 LTS Photon (includes Apache Spark 3.3.0, Scala 2.12)
     /// </summary>
     public const string Runtime_11_3_PHOTON = "11.3.x-photon-scala2.12";
+
+    #endregion
+
+    #region 12.0
+
+    /// <summary>
+    /// 12.0 (includes Apache Spark 3.3.1, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_0 = "12.0.x-scala2.12";
+
+    /// <summary>
+    /// 12.0 ML (includes Apache Spark 3.3.1, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_0_CPU_ML = "12.0.x-cpu-ml-scala2.12";
+
+    /// <summary>
+    /// 12.0 ML (includes Apache Spark 3.3.1, GPU, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_0_GPU_ML = "12.0.x-gpu-ml-scala2.12";
+
+    /// <summary>
+    /// 12.0 Photon (includes Apache Spark 3.3.1, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_0_PHOTON = "12.0.x-photon-scala2.12";
+
+    #endregion
+
+    #region 12.1
+
+    /// <summary>
+    /// 12.1 (includes Apache Spark 3.3.1, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_1 = "12.1.x-scala2.12";
+
+    /// <summary>
+    /// 12.1 ML (includes Apache Spark 3.3.1, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_1_CPU_ML = "12.1.x-cpu-ml-scala2.12";
+
+    /// <summary>
+    /// 12.1 ML (includes Apache Spark 3.3.1, GPU, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_1_GPU_ML = "12.1.x-gpu-ml-scala2.12";
+
+    /// <summary>
+    /// 12.1 Photon (includes Apache Spark 3.3.1, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_1_PHOTON = "12.1.x-photon-scala2.12";
+
+    #endregion
+
+    #region 12.2 LTS
+
+    /// <summary>
+    /// 12.2 LTS (includes Apache Spark 3.3.2, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_2 = "12.2.x-scala2.12";
+
+    /// <summary>
+    /// 12.2 LTS ML (includes Apache Spark 3.3.2, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_2_CPU_ML = "12.2.x-cpu-ml-scala2.12";
+
+    /// <summary>
+    /// 12.2 LTS ML (includes Apache Spark 3.3.2, GPU, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_2_GPU_ML = "12.2.x-gpu-ml-scala2.12";
+
+    /// <summary>
+    /// 12.2 LTS Photon (includes Apache Spark 3.3.2, Scala 2.12)
+    /// </summary>
+    public const string Runtime_12_2_PHOTON = "12.2.x-photon-scala2.12";
+
+    #endregion
+
+    #region 13.0
+
+    /// <summary>
+    /// 13.0 (includes Apache Spark 3.4.0, Scala 2.12)
+    /// </summary>
+    public const string Runtime_13_0 = "13.0.x-scala2.12";
+
+    /// <summary>
+    /// 13.0 ML (includes Apache Spark 3.4.0, Scala 2.12)
+    /// </summary>
+    public const string Runtime_13_0_CPU_ML = "13.0.x-cpu-ml-scala2.12";
+
+    /// <summary>
+    /// 13.0 ML (includes Apache Spark 3.4.0, GPU, Scala 2.12)
+    /// </summary>
+    public const string Runtime_13_0_GPU_ML = "13.0.x-gpu-ml-scala2.12";
+
+    /// <summary>
+    /// 13.0 Photon (includes Apache Spark 3.4.0, Scala 2.12)
+    /// </summary>
+    public const string Runtime_13_0_PHOTON = "13.0.x-photon-scala2.12";
 
     #endregion
 
