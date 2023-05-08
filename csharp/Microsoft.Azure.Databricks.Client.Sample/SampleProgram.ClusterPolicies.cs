d0a260b09bfc11cf21e49184f6214e193a5c408a
Jason Wang <jasowang@microsoft.com>
2023-05-08 01:19:02 -0700

    feat: add support for cluster policy family APIs (#129)
    Signed-off-by: Jason Wang <jasonwang_83@hotmail.com>

--- csharp/Microsoft.Azure.Databricks.Client.Sample/SampleProgram.ClusterPolicies.cs
+++ csharp/Microsoft.Azure.Databricks.Client.Sample/SampleProgram.ClusterPolicies.cs
@@ -10,26 +10,56 @@ internal static partial class SampleProgram
 {
     private static async Task TestClusterPoliciesApi(DatabricksClient client)
     {
+        Console.WriteLine("Listing policy families");
+        var policyFamilies = await client.ClusterPolicies.ListPolicyFamily(2);
+        foreach (var policyFamily in policyFamilies.Item1)
+        {
+            await Console.Out.WriteLineAsync($"\t{policyFamily.Name}");
+        }
+
+        policyFamilies = await client.ClusterPolicies.ListPolicyFamily(2, policyFamilies.Item2);
+        foreach (var policyFamily in policyFamilies.Item1)
+        {
+            await Console.Out.WriteLineAsync($"\t{policyFamily.Name}");
+        }
+
+        Console.WriteLine("Get policy family by id \"personal-vm\"");
+        var family = await client.ClusterPolicies.GetPolicyFamily("personal-vm");
+        await Console.Out.WriteLineAsync($"{family.Definition}");
+
         Console.WriteLine("Creating cluster policy");
         const string policyDef = "{\"spark_conf.spark.databricks.cluster.profile\":{\"type\":\"forbidden\",\"hidden\":true}}";
-        var policyId = await client.ClusterPolicies.Create("Test policy", policyDef);
+        var policyId1 = await client.ClusterPolicies.Create("Test policy 1", policyDef);
+
+        Console.WriteLine("Creating cluster policy with policy family");
+        const string policyFamilyOverride = "{\"spark_conf.spark.databricks.cluster.profile\":{\"type\":\"forbidden\",\"hidden\":true}}";
+        var policyId2 = await client.ClusterPolicies.CreateWithPoiclyFamily("Test policy 2", "personal-vm", policyFamilyOverride, 5);
 
         Console.WriteLine("Listing cluster policies...");
         var policies = await client.ClusterPolicies.List();
         foreach (var policy in policies)
         {
-            Console.WriteLine(
+            await Console.Out.WriteLineAsync(
                 $"\tPolicy {policy.Name}, Created at {policy.CreatedAtTimestamp}");
         }
 
         Console.WriteLine("Getting cluster policy");
-        var retrievedPolicy = await client.ClusterPolicies.Get(policyId);
-        Console.WriteLine($"\tPolicy {retrievedPolicy.Name}");
+        var retrievedPolicy1 = await client.ClusterPolicies.Get(policyId1);
+        Console.WriteLine($"\tPolicy {retrievedPolicy1.Name}");
 
-        Console.WriteLine($"Editing policy {policyId}");
-        await client.ClusterPolicies.Edit(policyId, "Test policy 2", policyDef);
+        var retrievedPolicy2 = await client.ClusterPolicies.Get(policyId2);
+        Console.WriteLine($"\tPolicy {retrievedPolicy2.Name}");
 
-        Console.WriteLine($"Deleting policy {policyId}");
-        await client.ClusterPolicies.Delete(policyId);
+        Console.WriteLine($"Editing policy {policyId1}");
+        await client.ClusterPolicies.Edit(policyId1, "Test policy 1", policyDef);
+
+        Console.WriteLine($"Editing policy {policyId2}");
+        await client.ClusterPolicies.EditWithPoiclyFamily(policyId2, "Test policy 2", "personal-vm");
+
+        Console.WriteLine($"Deleting policy {policyId1}");
+        await client.ClusterPolicies.Delete(policyId1);
+
+        Console.WriteLine($"Deleting policy {policyId2}");
+        await client.ClusterPolicies.Delete(policyId2);
     }
 }
---
+++ csharp/Microsoft.Azure.Databricks.Client/Models/PolicyFamily.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Azure.Databricks.Client.Models;
+
+public record PolicyFamily
+{
+    /// <summary>
+    /// ID of the policy family
+    /// </summary>
+    [JsonPropertyName("policy_family_id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Name of the policy family.
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Human-readable description of the purpose of the policy family.
+    /// </summary>
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Policy definition JSON document expressed in [Databricks Policy Definition Language](https://docs.microsoft.com/en-us/azure/databricks/administration-guide/clusters/policies#policy-def).
+    /// The JSON document must be passed as a string and cannot be embedded in the requests.
+    /// </summary>
+    [JsonPropertyName("definition")]
+    public string Definition { get; set; }
+
+    [JsonPropertyName("version")]
+    public int Version { get; set; }
+}
