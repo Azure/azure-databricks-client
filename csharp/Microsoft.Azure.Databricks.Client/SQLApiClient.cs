e129f0c4ed10b10795f78ce55b24dcba280c9c95
Jason Wang <jasonwang_83@hotmail.com>
2023-08-05 20:16:03 -0700

    SQL Warehouse API

---
+++ csharp/Microsoft.Azure.Databricks.Client/SQLApiClient.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Net.Http;
+
+namespace Microsoft.Azure.Databricks.Client
+{
+    public class SQLApiClient : ApiClient, ISQLApi
+    {
+        public SQLApiClient(HttpClient httpClient) : base(httpClient)
+        {
+            this.DataWarehouseApi = new DataWarehouseApiClient(httpClient);
+        }
+
+        public IDataWarehouseApi DataWarehouseApi { get; }
+    }
+}
