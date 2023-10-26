d75fece1c69c723dda4a22a5755f67337db026f4
Szymon Karpęcki <szymon.karpecki@dataedo.com>
2023-10-26 13:56:37 +0200

    Added UnityCatalog Schemas endpoint

---
+++ csharp/Microsoft.Azure.Databricks.Client/Models/UnityCatalog/Schema.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Databricks.Client.Models.UnityCatalog;
+
+public record Schema
+{
+    /// <summary>
+    /// Name of the schema, relative to the parent catalog.
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; init; }
+
+    /// <summary>
+    /// Name of the parent catalog.
+    /// </summary>
+    [JsonPropertyName("catalog_name")]
+    public string CatalogName { get; init; }
+
+    /// <summary>
+    /// Username of the current owner of the schema.
+    /// </summary>
+    [JsonPropertyName("owner")]
+    public string Owner { get; init; }
+
+    /// <summary>
+    /// User-provided free-form text description.
+    /// </summary>
+    [JsonPropertyName("comment")]
+    public string Comment { get; init; }
+
+    /// <summary>
+    /// A map of key-value properties attached to the securable.
+    /// </summary>
+    [JsonPropertyName("properties")]
+    public Dictionary<string, string> Properties { get; init; }
+
+    /// <summary>
+    /// Storage root URL for managed tables within the schema.
+    /// </summary>
+    [JsonPropertyName("storage_root")]
+    public string StorageRoot { get; init; }
+
+    /// <summary>
+    /// Unique identifier of the parent metastore.
+    /// </summary>
+    [JsonPropertyName("metastore_id")]
+    public string MetastoreId { get; init; }
+
+    /// <summary>
+    /// Full name of the schema, in the form of catalog_name.schema_name.
+    /// </summary>
+    [JsonPropertyName("full_name")]
+    public string FullName { get; init; }
+
+    /// <summary>
+    /// Storage location for managed tables within the schema.
+    /// </summary>
+    [JsonPropertyName("storage_location")]
+    public string StorageLocation { get; init; }
+
+    /// <summary>
+    /// Time at which this schema was created, in epoch milliseconds.
+    /// </summary>
+    [JsonPropertyName("created_at")]
+    public DateTimeOffset? CreatedAt { get; init; }
+
+    /// <summary>
+    /// Username of the schema creator.
+    /// </summary>
+    [JsonPropertyName("created_by")]
+    public string CreatedBy { get; init; }
+
+    /// <summary>
+    /// Time at which this schema was last modified, in epoch milliseconds.
+    /// </summary>
+    [JsonPropertyName("updated_at")]
+    public DateTimeOffset? UpdatedAt { get; init; }
+
+    /// <summary>
+    /// Username of the user who last modified the schema.
+    /// </summary>
+    [JsonPropertyName("updated_by")]
+    public string UpdatedBy { get; init; }
+
+    /// <summary>
+    /// The type of the parent catalog.
+    /// </summary>
+    [JsonPropertyName("catalog_type")]
+    public string CatalogType { get; init; }
+}
---
+++ csharp/Microsoft.Azure.Databricks.Client/UnityCatalog/SchemasApiClient.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Azure.Databricks.Client.Models.UnityCatalog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Databricks.Client.UnityCatalog;
+
+public class SchemasApiClient : ApiClient, ISchemasApi
+{
+    public SchemasApiClient(HttpClient httpClient) : base(httpClient)
+    {
+    }
+
+    public async Task<IEnumerable<Schema>> List(CancellationToken cancellationToken = default)
+    {
+        var requestUri = $"{BaseUnityCatalogUri}/schemas";
+        var schemasList = await HttpGet<JsonObject>(this.HttpClient, requestUri, cancellationToken).ConfigureAwait(false);
+
+        schemasList.TryGetPropertyValue("schemas", out var schemas);
+
+        return schemas?.Deserialize<IEnumerable<Schema>>(Options) ?? Enumerable.Empty<Schema>();
+    }
+
+    public async Task<Schema> Create(
+        string name,
+        string catalogName,
+        string comment = default,
+        Dictionary<string, string> properties = default,
+        string storageRoot = default,
+        CancellationToken cancellationToken = default)
+    {
+        var requestUri = $"{BaseUnityCatalogUri}/schemas";
+        var requestDict = new Dictionary<string, string>()
+        {
+            { "name", name },
+            { "catalog_name", catalogName }
+        };
+
+        if (comment != null)
+        {
+            requestDict["comment"] = comment;
+        }
+
+        if (storageRoot != null)
+        {
+            requestDict["storage_root"] = storageRoot;
+        }
+
+
+        var request = JsonSerializer.SerializeToNode(requestDict, Options).AsObject();
+        
+        if (properties != null)
+        {
+            request.Add("properties", JsonSerializer.SerializeToNode(properties, Options));
+        }
+
+        return await HttpPost<JsonObject, Schema>
+                (this.HttpClient, requestUri, request, cancellationToken)
+            .ConfigureAwait(false);
+
+    }
+
+    public async Task<Schema> Get(string schemaFullName, CancellationToken cancellationToken = default)
+    {
+        var requestUri = $"{BaseUnityCatalogUri}/schemas/{schemaFullName}";
+        return await HttpGet<Schema>(this.HttpClient, requestUri, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task<Schema> Update(
+        string schemaFullName,
+        string name = default,
+        string owner = default,
+        string comment = default,
+        Dictionary<string, string> properties = default,
+        CancellationToken cancellationToken = default)
+    {
+        var requestUri = $"{BaseUnityCatalogUri}/schemas/{schemaFullName}";
+
+        var requestDict = new Dictionary<string, string>();
+        
+        if (name != null)
+        {
+            requestDict["name"] = name;
+        }
+
+        if (owner != null)
+        {
+            requestDict["owner"] = owner;
+        }
+
+        if (comment != null)
+        {
+            requestDict["comment"] = comment;
+        }
+
+        var request = JsonSerializer.SerializeToNode(requestDict, Options).AsObject();
+
+        if (properties != null)
+        {
+            request.Add("properties", JsonSerializer.SerializeToNode(properties, Options));
+        }
+
+        return await HttpPatch<JsonObject, Schema>(HttpClient, requestUri, request, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task Delete(string schemaFullName, CancellationToken cancellationToken = default)
+    {
+        var requestUri = $"{BaseUnityCatalogUri}/schemas/{schemaFullName}";
+
+        await HttpDelete(HttpClient, requestUri, cancellationToken).ConfigureAwait(false);
+    }
+}
