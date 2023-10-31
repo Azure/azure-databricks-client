6aa97ddc5cf2aca1f5097042aaedc8e0eb6639bd
Szymon Karpęcki <szymon.karpecki@dataedo.com>
2023-10-31 10:37:24 +0100

    Added SecurableWorkspaceBinding UnityCatalog endpoint

---
+++ csharp/Microsoft.Azure.Databricks.Client/Models/UnityCatalog/SecurableWorkspaceBinding.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Microsoft.Azure.Databricks.Client.Models.UnityCatalog;
+
+public record SecurableWorkspaceBinding
+{
+    [JsonPropertyName("workspace_id")]
+    public long WorkspaceId { get; set; }
+
+    [JsonPropertyName("binding_type")]
+    public BindingType? BindingType { get; set; }
+}
+
+public enum BindingType
+{
+    BINDING_TYPE_READ_WRITE,
+    BINDING_TYPE_READ_ONLY
+}
---
+++ csharp/Microsoft.Azure.Databricks.Client/UnityCatalog/ISecurableWorkspaceBindingsApi.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Databricks.Client.Models.UnityCatalog;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Databricks.Client.UnityCatalog;
+
+public interface ISecurableWorkspaceBindingsApi : IDisposable
+{
+    /// <summary>
+    /// Gets workspace bindings of the securable. The caller must be a metastore admin or an owner of the securable.
+    /// </summary>
+    Task<IEnumerable<SecurableWorkspaceBinding>> Get(
+        string securableType,
+        string securableName,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Updates workspace bindings of the securable. The caller must be a metastore admin or an owner of the securable.
+    /// </summary>
+    Task<IEnumerable<SecurableWorkspaceBinding>> Update(
+        string securableType,
+        string securableName,
+        IEnumerable<SecurableWorkspaceBinding> add,
+        IEnumerable<SecurableWorkspaceBinding> remove,
+        CancellationToken cancellationToken = default);
+}
