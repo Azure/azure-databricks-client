90fe4bcef841f173318c8282eb40f2b30323099f
SjoerdSjoerd <Sjoerdsjoerd@users.noreply.github.com>
2023-11-27 11:13:39 +0100

    Moved job notification settings to BaseClass

--- csharp/Microsoft.Azure.Databricks.Client/Models/JobSettings.cs
+++ csharp/Microsoft.Azure.Databricks.Client/Models/JobSettings.cs
@@ -16,6 +16,18 @@ public abstract record JobRunBaseSettings<TTaskSetting> : IJsonOnDeserialized
     [JsonPropertyName("tasks")]
     public List<TTaskSetting> Tasks { get; set; } = new();
 
+    /// <summary>
+    /// An optional set of email addresses that will be notified when runs of this job begin or complete as well as when this job is deleted. The default behavior is to not send any emails.
+    /// </summary>
+    [JsonPropertyName("email_notifications")]
+    public JobEmailNotifications EmailNotifications { get; set; }
+
+    /// <summary>
+    /// An optional set of webhook addresses that will be notified when runs of this job begin or complete as well as when this job is deleted. The default behavior is to not send any webhook notifications.
+    /// </summary>
+    [JsonPropertyName("webhook_notifications")]
+    public JobWebhookNotifications WebhookNotifications { get; set; }
+
     public void OnDeserialized()
     {
         var taskMap = this.Tasks.ToDictionary(
@@ -186,18 +198,6 @@ public record JobSettings : JobRunBaseSettings<JobTaskSettings>
     public IEnumerable<JobCluster> JobClusters { get; set; }
 
     /// <summary>
-    /// An optional set of email addresses that will be notified when runs of this job begin or complete as well as when this job is deleted. The default behavior is to not send any emails.
-    /// </summary>
-    [JsonPropertyName("email_notifications")]
-    public JobEmailNotifications EmailNotifications { get; set; }
-
-    /// <summary>
-    /// An optional set of webhook addresses that will be notified when runs of this job begin or complete as well as when this job is deleted. The default behavior is to not send any webhook notifications.
-    /// </summary>
-    [JsonPropertyName("webhook_notifications")]
-    public JobWebhookNotifications WebhookNotifications { get; set; }
-
-    /// <summary>
     /// An optional periodic schedule for this job. The default behavior is that the job only runs when triggered by clicking "Run Now" in the Jobs UI or sending an API request to `runNow`.
     /// </summary>
     [JsonPropertyName("schedule")]
@@ -235,16 +235,4 @@ public record RunSubmitSettings : JobRunBaseSettings<RunSubmitTaskSettings>
     /// </summary>
     [JsonPropertyName("run_name")]
     public string RunName { get; set; }
-
-    /// <summary>
-    /// An optional set of email addresses that will be notified when runs of this job begin or complete as well as when this job is deleted. The default behavior is to not send any emails.
-    /// </summary>
-    [JsonPropertyName("email_notifications")]
-    public JobEmailNotifications EmailNotifications { get; set; }
-
-    /// <summary>
-    /// An optional set of webhook addresses that will be notified when runs of this job begin or complete as well as when this job is deleted. The default behavior is to not send any webhook notifications.
-    /// </summary>
-    [JsonPropertyName("webhook_notifications")]
-    public JobWebhookNotifications WebhookNotifications { get; set; }
 }
