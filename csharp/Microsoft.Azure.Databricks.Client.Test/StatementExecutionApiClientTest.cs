0b09e4c69ee155e1930453d6bc62e3a49e31fb69
Adam Weigert <smokedlinq@outlook.com>
2024-03-28 12:52:42 -0400

    Add Statement Execution API

---
+++ csharp/Microsoft.Azure.Databricks.Client.Test/StatementExecutionApiClientTest.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Databricks.Client.Models;
+using Moq;
+using Moq.Contrib.HttpClient;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Microsoft.Azure.Databricks.Client.Test;
+
+[TestClass]
+public class StatementExecutionApiClientTest : ApiClientTest
+{
+    private static readonly Uri StatementExecutionApiUri = new(BaseApiUri, "2.0/sql/statements");
+
+    [TestMethod]
+    public async Task TestExecute()
+    {
+        const string expectedRequest = "{\"statement\":\"string\",\"warehouse_id\":\"string\",\"parameters\":[]}";
+        const string expectedResponse = @"
+                {
+                  ""statement_id"": ""string"",
+                  ""status"": {
+                        ""state"": ""SUCCEEDED""
+                  },
+                  ""manifest"": {
+                      ""format"": ""JSON_ARRAY"",
+                      ""schema"": {
+                          ""column_count"": 1,
+                          ""columns"": [
+                              {
+                                  ""name"": ""string"",
+                                  ""position"": 0,
+                                  ""type_name"": ""string"",
+                                  ""type_text"": ""string""
+                              }
+                          ]
+                      }
+                  },
+                  ""result"": {
+                      ""chunk_index"": 0,
+                      ""row_offset"": 0,
+                      ""row_count"": 1,
+                      ""data_array"": [
+                        [ ""0"" ]
+                      ]
+                  }
+                }
+                ";
+
+        var expected = JsonSerializer.Deserialize<StatementExecution>(expectedResponse, Options);
+
+        var handler = CreateMockHandler();
+        handler
+            .SetupRequest(HttpMethod.Post, StatementExecutionApiUri)
+            .ReturnsResponse(HttpStatusCode.OK, JsonSerializer.Serialize(expected, Options), "application/json")
+            .Verifiable();
+
+        var hc = handler.CreateClient();
+        hc.BaseAddress = BaseApiUri;
+
+        using var client = new StatementExecutionApiClient(hc);
+        var statement = JsonNode.Parse(expectedRequest).Deserialize<SqlStatement>(Options);
+
+        var actual = await client.Execute(statement);
+        Assert.IsTrue(expected.Equals(actual));
+
+        handler.VerifyRequest(
+            HttpMethod.Post,
+            StatementExecutionApiUri,
+            GetMatcher(expectedRequest),
+            Times.Once()
+        );
+    }
+
+    [TestMethod]
+    public async Task TestGet()
+    {
+        string testId = "1234-567890-cited123";
+        string apiUri = $"{StatementExecutionApiUri}/{testId}";
+
+        const string expectedResponse = @"
+                {
+                  ""statement_id"": ""string"",
+                  ""status"": {
+                        ""state"": ""SUCCEEDED""
+                  },
+                  ""manifest"": {
+                      ""format"": ""JSON_ARRAY"",
+                      ""schema"": {
+                          ""column_count"": 1,
+                          ""columns"": [
+                              {
+                                  ""name"": ""string"",
+                                  ""position"": 0,
+                                  ""type_name"": ""string"",
+                                  ""type_text"": ""string""
+                              }
+                          ]
+                      }
+                  },
+                  ""result"": {
+                      ""chunk_index"": 0,
+                      ""row_offset"": 0,
+                      ""row_count"": 1,
+                      ""data_array"": [
+                        [ ""0"" ]
+                      ]
+                  }
+                }
+                ";
+
+        var expected = JsonSerializer.Deserialize<StatementExecution>(expectedResponse, Options);
+
+        var handler = CreateMockHandler();
+        handler
+            .SetupRequest(HttpMethod.Get, apiUri)
+            .ReturnsResponse(HttpStatusCode.OK, expectedResponse, "application/json");
+
+        var hc = handler.CreateClient();
+        hc.BaseAddress = BaseApiUri;
+
+        using var client = new StatementExecutionApiClient(hc);
+        var actual = await client.Get(testId);
+        Assert.IsTrue(expected.Equals(actual));
+    }
+
+    [TestMethod]
+    public async Task TestCancel()
+    {
+        string testId = "1234-567890-cited123";
+        string apiUri = $"{StatementExecutionApiUri}/{testId}/cancel";
+        var handler = CreateMockHandler();
+        handler
+            .SetupRequest(HttpMethod.Post, apiUri)
+            .ReturnsResponse(HttpStatusCode.OK)
+            .Verifiable();
+
+        var hc = handler.CreateClient();
+        hc.BaseAddress = BaseApiUri;
+
+        using var client = new StatementExecutionApiClient(hc);
+        await client.Cancel(testId);
+        handler.VerifyRequest(
+            HttpMethod.Post,
+            apiUri,
+            Times.Once()
+        );
+    }
+
+    [TestMethod]
+    public async Task TestGetResultChunk()
+    {
+        string testId = "1234-567890-cited123";
+        int chunkIndex = 0;
+        string apiUri = $"{StatementExecutionApiUri}/{testId}/result/chunks/{chunkIndex}";
+
+        const string expectedResponse = @"
+                {
+                    ""chunk_index"": 0,
+                    ""row_offset"": 0,
+                    ""row_count"": 1,
+                    ""data_array"": [
+                        [ ""0"" ]
+                    ]
+                }
+                ";
+
+        var expected = JsonSerializer.Deserialize<StatementExecutionResultChunk>(expectedResponse, Options);
+
+        var handler = CreateMockHandler();
+        handler
+            .SetupRequest(HttpMethod.Get, apiUri)
+            .ReturnsResponse(HttpStatusCode.OK, expectedResponse, "application/json");
+
+        var hc = handler.CreateClient();
+        hc.BaseAddress = BaseApiUri;
+
+        using var client = new StatementExecutionApiClient(hc);
+        var actual = await client.GetResultChunk(testId, chunkIndex);
+        Assert.IsTrue(expected.Equals(actual));
+    }
+}
---
+++ csharp/Microsoft.Azure.Databricks.Client/StatementExecutionApiClient.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Databricks.Client.Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Databricks.Client
+{
+    public class StatementExecutionApiClient : ApiClient, IStatementExecutionApi
+    {
+        private readonly string _apiBaseUrl;
+
+        public StatementExecutionApiClient(HttpClient httpClient) : base(httpClient)
+        {
+            _apiBaseUrl = $"{ApiVersion}/sql/statements";
+        }
+
+        public async Task Cancel(string id, CancellationToken cancellationToken = default)
+        {
+            await HttpPost(this.HttpClient, $"{this._apiBaseUrl}/{id}/cancel", new { }, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<StatementExecution> Execute(SqlStatement statement, CancellationToken cancellationToken = default)
+        {
+            var jsonObj = JsonSerializer.SerializeToNode(statement, Options)!.AsObject();
+
+            var execution = await HttpPost<JsonObject, JsonObject>(
+                this.HttpClient,
+                this._apiBaseUrl,
+                jsonObj,
+                cancellationToken
+            ).ConfigureAwait(false);
+
+            return execution.Deserialize<StatementExecution>(Options);
+        }
+
+        public async Task<StatementExecutionResultChunk> GetResultChunk(string id, int chunkIndex, CancellationToken cancellationToken = default)
+        {
+            var execution = await HttpGet<JsonObject>(
+                this.HttpClient,
+                $"{this._apiBaseUrl}/{id}/result/chunks/{chunkIndex}",
+                cancellationToken
+            ).ConfigureAwait(false);
+
+            return execution.Deserialize<StatementExecutionResultChunk>(Options);
+        }
+
+        public async Task<StatementExecution> Get(string id, CancellationToken cancellationToken = default)
+        {
+            var execution = await HttpGet<JsonObject>(
+                this.HttpClient,
+                $"{this._apiBaseUrl}/{id}",
+                cancellationToken
+            ).ConfigureAwait(false);
+
+            return execution.Deserialize<StatementExecution>(Options);
+        }
+    }
+}
