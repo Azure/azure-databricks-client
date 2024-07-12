e2fb5a30ca8588ddb3e68ede6c087f4301c31a5b
Hugh Signoriello <hugh.signoriello@insightfactory.ai>
2024-07-12 15:46:38 +0930

    Add sample programAdd format whitespace and update ML endpointAdd Machine Learning Experiment Api endpoint and test

---
+++ csharp/Microsoft.Azure.Databricks.Client.Sample/SampleProgram .MachineLearning.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Databricks.Client.Models.UnityCatalog;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Databricks.Client.Sample;
+
+internal static partial class SampleProgram
+{
+    private static async Task TestExperimentApiClient(DatabricksClient client)
+    {
+        string run_id = "sample_run_id";
+        PrintDelimiter();
+        Console.WriteLine("Listing metastores");
+        var data = await client.MachineLearning.Experiments.GetRun(run_id);
+        Console.WriteLine($"\t{data.Info.RunId}");
+        PrintDelimiter();
+
+    }
+}
