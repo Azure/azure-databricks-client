9cb3a61c5f2bbf3535b53f69df72299b47083d97
Pouya <botbot40735@gmail.com>
2025-05-31 16:50:36 +0930

    Add import workspace test

--- csharp/Microsoft.Azure.Databricks.Client.Test/WorkspaceApiClientTest.cs
+++ csharp/Microsoft.Azure.Databricks.Client.Test/WorkspaceApiClientTest.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.Databricks.Client.Models;
 using Moq;
 using Moq.Contrib.HttpClient;
 using System.Net;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
@@ -44,6 +45,43 @@ public class WorkspaceApiClientTest : ApiClientTest
         handler.VerifyRequest(
             HttpMethod.Get,
             new Uri(apiUri, "?path=/notebook_path&format=SOURCE"),
+            Times.Once()
+        );
+    }
+
+    [TestMethod]
+    public async Task TestImport()
+    {
+        var apiUri = new Uri(WorkspaceApiUri, "import");
+
+        var content = "Ly8gRGF0YWJyaWNrcyBub3RlYm9vayBzb3VyY2UKMSsx";
+        var contentBytes = Encoding.ASCII.GetBytes(content);
+        var contentBase64 = Convert.ToBase64String(contentBytes);
+
+        string expectedRequest = $@"
+            {{
+              ""content"": ""{contentBase64}"",
+              ""format"": ""AUTO"",
+              ""language"": ""PYTHON"",
+              ""path"": ""/notebook_path"",
+              ""overwrite"": false
+            }}";
+
+        var handler = CreateMockHandler();
+        handler
+            .SetupRequest(HttpMethod.Post, apiUri)
+            .ReturnsResponse(HttpStatusCode.OK);
+
+        var hc = handler.CreateClient();
+        hc.BaseAddress = BaseApiUri;
+
+        using var client = new WorkspaceApiClient(hc);
+        await client.Import("/notebook_path", ExportFormat.AUTO, Language.PYTHON, contentBytes, false);
+
+        handler.VerifyRequest(
+            HttpMethod.Post,
+            apiUri,
+            GetMatcher(expectedRequest),
             Times.Once()
         );
     }
